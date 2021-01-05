6e0a05857cba5c8ddb15a1ed1f8c9fb0e36d97bf
Resi Respati <resir014@gmail.com>
2021-01-05 17:05:04 +0700

    refactor(chungking-react): restyle `MessageBox` component (#2)

--- packages/chungking-react/src/components/message-box/MessageBox.tsx
+++ packages/chungking-react/src/components/message-box/MessageBox.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { css } from '@emotion/react'
+import { variant } from 'styled-system'
 import styled from '@emotion/styled'
 import { transparentize } from 'polished'
 import { colors, space } from '../../utils'
@@ -7,39 +7,54 @@ import { Box, BoxProps } from '../../foundations'
 
 export interface MessageBoxProps extends BoxProps {
   className?: string
-  variant?: 'default' | 'warning'
+  variant?: 'default' | 'primary' | 'success' | 'warning' | 'error'
   children?: React.ReactNode
 }
 
-const DefaultStyles = css`
-  border-color: ${colors.blue[500]};
-  background-color: ${transparentize(0.75, colors.blue[500])};
-
-  a {
-    color: ${colors.turquoise[400]};
-  }
-`
-
-const WarningStyles = css`
-  border-color: ${colors.red[500]};
-  background-color: ${transparentize(0.75, colors.red[500])};
-
-  a {
-    color: ${colors.orange[400]};
-  }
-`
-
 const Root = styled(Box)<MessageBoxProps>`
   padding: ${space.md}px;
-  border: 2px solid transparent;
-  border-radius: 4px;
+  border-left: 2px solid transparent;
 
-  a {
-    color: ${colors.turquoise[400]};
-  }
-
-  ${(props) => props.variant === 'default' && DefaultStyles}
-  ${(props) => props.variant === 'warning' && WarningStyles}
+  ${variant({
+    prop: 'variant',
+    variants: {
+      default: {
+        borderLeftColor: 'grey.500',
+        backgroundColor: transparentize(0.75, colors.grey[500]),
+        '& a': {
+          color: 'turquoise.400'
+        }
+      },
+      primary: {
+        borderLeftColor: 'blue.500',
+        backgroundColor: transparentize(0.75, colors.blue[500]),
+        '& a': {
+          color: 'turquoise.400'
+        }
+      },
+      success: {
+        borderLeftColor: 'green.500',
+        backgroundColor: transparentize(0.75, colors.green[500]),
+        '& a': {
+          color: 'turquoise.400'
+        }
+      },
+      warning: {
+        borderLeftColor: 'orange.500',
+        backgroundColor: transparentize(0.75, colors.orange[500]),
+        '& a': {
+          color: 'orange.400'
+        }
+      },
+      error: {
+        borderLeftColor: 'red.500',
+        backgroundColor: transparentize(0.75, colors.red[500]),
+        '& a': {
+          color: 'orange.400'
+        }
+      }
+    }
+  })}
 `
 
 const MessageBox: React.FC<MessageBoxProps> = ({ className, children, ...rest }) => (
