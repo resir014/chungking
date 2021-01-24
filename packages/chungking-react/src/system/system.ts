9788248f4bc07a61c687af89f9fec566c45755bf
Resi Respati <resir014@gmail.com>
2021-01-24 21:27:19 +0700

    refactor(chungking-react): minor refactor to style props

--- packages/chungking-react/src/system/system.ts
+++ packages/chungking-react/src/system/system.ts
@@ -1,19 +1,16 @@
-import { system } from 'styled-system'
+import * as CSS from 'csstype'
+import { Theme, RequiredTheme, system, ResponsiveValue } from 'styled-system'
 
-export interface TextDecorationProps {
-  textDecoration?: string | string[]
+export interface TextDecorationProps<ThemeType extends Theme = RequiredTheme> {
+  textDecoration?: ResponsiveValue<CSS.Property.TextDecoration, ThemeType>
 }
 
-export const textDecoration = system({
-  prop: 'textDecoration',
-  cssProperty: 'textDecoration'
-} as any)
+export const textDecoration = system({ textDecoration: true })
 
-export interface TextTransformProps {
-  textTransform?: string | string[]
+export interface TextTransformProps<ThemeType extends Theme = RequiredTheme> {
+  textTransform?: ResponsiveValue<CSS.Property.TextTransform, ThemeType>
 }
 
 export const textTransform = system({
-  prop: 'textTransform',
-  cssProperty: 'textTransform'
-} as any)
+  textTransform: true
+})
