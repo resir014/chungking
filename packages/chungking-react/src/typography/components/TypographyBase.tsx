1886745e86f4611591f8cbc4b27819bcd8cf8c68
Resi Respati <resir014@gmail.com>
2021-02-16 00:16:33 +0700

    refactor(react): refactor theme, use `useComponentStyles` util

--- packages/chungking-react/src/typography/components/TypographyBase.tsx
+++ packages/chungking-react/src/typography/components/TypographyBase.tsx
@@ -3,7 +3,8 @@ import * as React from 'react'
 import styled from '@emotion/styled'
 import { shouldForwardProp } from '@spicy-ui/styled-system'
 
-import { AllSystemProps, allSystemProps, pseudo, PseudoProps, variant } from '../../system'
+import { AllSystemProps, allSystemProps, pseudo, PseudoProps } from '../../system'
+import { getComponentStyles } from '../../system/utils/components'
 
 export type TypeScale = 900 | 800 | 700 | 600 | 500 | 400 | 300 | 200 | 100
 
@@ -21,13 +22,6 @@ export interface TypographyBaseProps extends AllSystemProps, PseudoProps {
 /**
  * This is a base `Text` element to handle typography elements.
  */
-export const TypographyBase = styled('span', { shouldForwardProp })<TypographyBaseProps>(
-  variant({
-    prop: 'variant',
-    scale: 'components.Text.scales.variants'
-  }),
-  allSystemProps,
-  pseudo
-)
+export const TypographyBase = styled('span', { shouldForwardProp })<TypographyBaseProps>(getComponentStyles('text'), allSystemProps, pseudo)
 
 TypographyBase.displayName = 'TypographyBase'
