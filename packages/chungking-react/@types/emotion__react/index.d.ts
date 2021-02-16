27c1284081a66a5fe59dee3aa4c2e2f6d9f2abf1
Resi Respati <resir014@gmail.com>
2021-02-16 21:47:22 +0700

    fix(react): fixed build errors

--- packages/chungking-react/@types/emotion__react/index.d.ts
+++ packages/chungking-react/@types/emotion__react/index.d.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import '@emotion/react'
-import { theme } from '../../theme'
+import { theme } from '../../src/theme'
 
 type DefaultTheme = typeof theme
 
