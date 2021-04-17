96925562e0c8cabf510d9c1ee4183f342d1f10a0
Resi Respati <resir014@gmail.com>
2021-04-17 22:06:44 +0700

    fix(chungking-react): fixed heading types

--- packages/chungking-react/src/typography/components/Heading.tsx
+++ packages/chungking-react/src/typography/components/Heading.tsx
@@ -4,12 +4,16 @@ import { allSystemProps, pseudo } from '../../system'
 import { getComponentStyles } from '../../system/utils/components'
 import { TypographyBaseProps } from './TypographyBase'
 
-export type HeadingProps = TypographyBaseProps
+export type HeadingScale = 900 | 800 | 700 | 600 | 500 | 400 | 300 | 200 | 100
+
+export interface HeadingProps extends TypographyBaseProps {
+  variant?: HeadingScale
+}
 
 /**
  * Heading component provided as a styled component primitive.
  */
-const Heading = styled('h2', { shouldForwardProp })<TypographyBaseProps>(getComponentStyles('heading'), allSystemProps, pseudo)
+const Heading = styled('h2', { shouldForwardProp })<HeadingProps>(getComponentStyles('heading'), allSystemProps, pseudo)
 
 Heading.defaultProps = {
   variant: 800,
