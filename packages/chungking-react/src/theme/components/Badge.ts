52f3c9edfe2a709e5efdec8124208678fd8d712a
Resi Respati <resir014@gmail.com>
2021-04-17 23:51:19 +0700

    refactor(chungking-react): re-theme badge component

---
+++ packages/chungking-react/src/theme/components/Badge.ts
@@ -0,0 +1,42 @@
+import { transparentize } from 'polished'
+import { ComponentThemeConfig } from '../types'
+
+const Badge: ComponentThemeConfig = {
+  propToScaleMap: [['variant', 'variants']],
+  baseStyle: () => ({
+    display: 'inline-flex',
+    alignItems: 'center',
+    px: 'xxs',
+    fontSize: 'sm',
+    fontWeight: 500,
+    lineHeight: 'short',
+    border: '1px solid transparent',
+    borderRadius: 4
+  }),
+  scales: {
+    variants: {
+      outline: ({ colorScheme: c, theme }) => {
+        const darkScheme = transparentize(0.2, theme.colors[c][200])
+        return {
+          borderColor: darkScheme,
+          color: darkScheme
+        }
+      },
+      solid: ({ colorScheme: c, theme }) => {
+        const darkScheme = transparentize(0.6, theme.colors[c][500])
+        return {
+          backgroundColor: darkScheme,
+          color: 'white'
+        }
+      },
+      subtle: ({ colorScheme: c, theme }) => {
+        return {
+          backgroundColor: transparentize(0.85, theme.colors[c][200]),
+          color: `${c}.200`
+        }
+      }
+    }
+  }
+}
+
+export default Badge
--- packages/chungking-react/src/theme/components/index.ts
+++ packages/chungking-react/src/theme/components/index.ts
@@ -1,7 +1,9 @@
+import Badge from './Badge'
 import Card from './Card'
 import * as Typography from './Typography'
 
 const components = {
+  Badge,
   Card,
   ...Typography
 }
