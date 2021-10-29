1e3fef270c0da7f7fffac3756005d873c2dd9d8d
Resi Respati <resir014@gmail.com>
2021-10-29 21:51:09 +0700

    chore: use tsconfig paths as webpack aliases in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,18 @@ const path = require('path')
 
 const toPath = (_path) => path.join(process.cwd(), _path)
 
+// https://github.com/radix-ui/primitives/blob/e30fccc1769721041293ee3475d81ccd6394bbfa/.storybook/main.js#L21-L30
+function convertTsConfigPathsToWebpackAliases() {
+  const rootDir = path.resolve(__dirname, '../')
+  const tsconfig = require('../tsconfig.json')
+  const tsconfigPaths = Object.entries(tsconfig.compilerOptions.paths)
+
+  return tsconfigPaths.reduce((aliases, [realPath, mappedPath]) => {
+    aliases[realPath] = path.join(rootDir, mappedPath[0])
+    return aliases
+  }, {})
+}
+
 module.exports = {
   stories: ['../packages/chungking-react/**/*.stories.mdx', '../packages/chungking-react/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -13,6 +25,7 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
+          ...convertTsConfigPathsToWebpackAliases(),
           // TODO: Remove once Storybook supports Emotion 11.
           '@emotion/core': toPath('node_modules/@emotion/react'),
           '@emotion/styled': toPath('node_modules/@emotion/styled'),
