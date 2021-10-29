d5ba5b5648144138d393d9250722c52ab16aec40
Resi Respati <resir014@gmail.com>
2021-10-29 21:12:44 +0700

    chore: move storybook to root

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const path = require('path')
 const toPath = (_path) => path.join(process.cwd(), _path)
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../packages/chungking-react/**/*.stories.mdx', '../packages/chungking-react/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://github.com/storybookjs/storybook/issues/10231
   webpackFinal: async (config) => {
@@ -14,9 +14,9 @@ module.exports = {
         alias: {
           ...config.resolve.alias,
           // TODO: Remove once Storybook supports Emotion 11.
-          '@emotion/core': toPath('../../node_modules/@emotion/react'),
-          '@emotion/styled': toPath('../../node_modules/@emotion/styled'),
-          'emotion-theming': toPath('../../node_modules/@emotion/react')
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+          'emotion-theming': toPath('node_modules/@emotion/react')
         }
       }
     }
