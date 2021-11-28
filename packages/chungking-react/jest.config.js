9f977698f41853cccea8d8c74eb266f45ae27d55
Resi Respati <resir014@gmail.com>
2021-11-28 10:24:18 +0700

    chore: start migrating all eslint/jest stuff to esm

--- packages/chungking-react/jest.config.js
+++ packages/chungking-react/jest.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-  preset: 'ts-jest',
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  // transform typescript files with esbuild-jest
+  transform: {
+    '^.+\\.tsx?$': ['esbuild-jest', { sourcemap: true, format: 'esm' }]
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
   collectCoverageFrom: [
@@ -11,3 +16,5 @@ module.exports = {
     '!src/index.{ts,tsx}'
   ]
 }
+
+export default config
